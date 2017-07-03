664c3c86c4719132dc305231ceb31a7225f2f429
Diyanasekar <diyanas1997@gmail.com>
2017-07-03 14:18:48 +0530

    Rename README.md to Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+# project6
+import java.io.*;
+import java.util.*;
+public class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(system.in);
+for(int i=0;i<=5;i++)
+{
+System.out.println("hello");
+}
+}
+}
+
+
